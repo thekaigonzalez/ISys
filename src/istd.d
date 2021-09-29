5804b53fde401a5e980580dfa8f0c20607cd46d1
Kai Gonzalez <gkai70264@gmail.com>
2021-09-29 17:07:02 -0400

    More STD Functions

--- src/istd.d
+++ src/istd.d
@@ -1,10 +1,15 @@
 import ivm;
 import igc;
 import std.stdio;
+import std.digest: toHexString;
+import core.stdc.stdlib;
 import std.algorithm.mutation;
 import iarg;
 import isyslib;
 import std.string;
+
+/// contains global variables
+string[] glob;
 
 import ilex;
 ///standard library
@@ -28,18 +33,27 @@ class Fstd {
 		/// 1: function exists, run success
 		/// 0: function doesn't exist, or run not successful
 		int runFunc() {
-			writeln(ls.key());
+			
 			if (ls.key() == "LEN") {
-				writeln(as.checkdynstring(0).length);
+				writeln(as.checkdynstring(1).length);
 				return 1;
 			} else if (ls.key() == "REV") {
 				writeln(ISys_Vsmush(as.checkdynstring(0).split.reverse));
 				return 1;
 			} else if (ls.key() == "BYTE") {
 				/// memory
-				int fmem = as.checkNum();
+				const int fmem = as.checkNum();
 				malloc(fmem);
 				return 1;	
+			} else if (ls.key() == "DUMP") {
+				writeln(malloc(as.checkword().length));
+				return 1;
+			} else if (ls.key() == "HEX") {
+				(cast(ubyte[]) as.checkword())    // We want raw bytes...
+        		.toHexString     // ...convert them to hex...
+        		.writeln;        // ...and print.
+
+				return 1;
 			} else {
 				return -1;
 			}
